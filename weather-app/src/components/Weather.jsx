7bef61abad71491b3365be74b3a5c9dac54fc6ad
Dimitris Agtzidis <agtzdimi@gmail.com>
2018-10-26 15:50:14 +0300

    [MODIFIED] #7 renamed the images to remove multiple switch cases

--- weather-app/src/components/Weather.jsx
+++ weather-app/src/components/Weather.jsx
@@ -23,107 +23,14 @@ class Weather extends Component {
   clockFormat = (cell, row) => {
     const clockPath = "./img/clock/";
     const clockFormat = "width=64 height=64>";
-
-    switch (cell) {
-      case "00:00":
-        return (
-          cell + '<img src="' + clockPath + "0 o'clock.png\"" + clockFormat
-        );
-      case "01:00":
-        return (
-          cell + '<img src="' + clockPath + "1 o'clock.png\"" + clockFormat
-        );
-      case "02:00":
-        return (
-          cell + '<img src="' + clockPath + "2 o'clock.png\"" + clockFormat
-        );
-      case "03:00":
-        return (
-          cell + '<img src="' + clockPath + "3 o'clock.png\"" + clockFormat
-        );
-      case "04:00":
-        return (
-          cell + '<img src="' + clockPath + "4 o'clock.png\"" + clockFormat
-        );
-      case "05:00":
-        return (
-          cell + '<img src="' + clockPath + "5 o'clock.png\"" + clockFormat
-        );
-      case "06:00":
-        return (
-          cell + '<img src="' + clockPath + "6 o'clock.png\"" + clockFormat
-        );
-      case "07:00":
-        return (
-          cell + '<img src="' + clockPath + "7 o'clock.png\"" + clockFormat
-        );
-      case "08:00":
-        return (
-          cell + '<img src="' + clockPath + "8 o'clock.png\"" + clockFormat
-        );
-      case "09:00":
-        return (
-          cell + '<img src="' + clockPath + "9 o'clock.png\"" + clockFormat
-        );
-      case "10:00":
-        return (
-          cell + '<img src="' + clockPath + "10 o'clock.png\"" + clockFormat
-        );
-      case "11:00":
-        return (
-          cell + '<img src="' + clockPath + "11 o'clock.png\"" + clockFormat
-        );
-      case "12:00":
-        return (
-          cell + '<img src="' + clockPath + "12 o'clock.png\"" + clockFormat
-        );
-      case "13:00":
-        return (
-          cell + '<img src="' + clockPath + "13 o'clock.png\"" + clockFormat
-        );
-      case "14:00":
-        return (
-          cell + '<img src="' + clockPath + "14 o'clock.png\"" + clockFormat
-        );
-      case "15:00":
-        return (
-          cell + '<img src="' + clockPath + "15 o'clock.png\"" + clockFormat
-        );
-      case "16:00":
-        return (
-          cell + '<img src="' + clockPath + "16 o'clock.png\"" + clockFormat
-        );
-      case "17:00":
-        return (
-          cell + '<img src="' + clockPath + "17 o'clock.png\"" + clockFormat
-        );
-      case "18:00":
-        return (
-          cell + '<img src="' + clockPath + "18 o'clock.png\"" + clockFormat
-        );
-      case "19:00":
-        return (
-          cell + '<img src="' + clockPath + "19 o'clock.png\"" + clockFormat
-        );
-      case "20:00":
-        return (
-          cell + '<img src="' + clockPath + "20 o'clock.png\"" + clockFormat
-        );
-      case "21:00":
-        return (
-          cell + '<img src="' + clockPath + "21 o'clock.png\"" + clockFormat
-        );
-      case "22:00":
-        return (
-          cell + '<img src="' + clockPath + "22 o'clock.png\"" + clockFormat
-        );
-      case "23:00":
-        return (
-          cell + '<img src="' + clockPath + "23 o'clock.png\"" + clockFormat
-        );
-      default:
-        return cell;
-    }
+    return (
+      cell +
+      '<img src="' +
+      clockPath +
+      cell.substr(0, 2) +
+      " o'clock.png\"" +
+      clockFormat
+    );
   };
 
   expandFormat = cell => {
